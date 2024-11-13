42f65b90a0fda0404110c65ea5bd06b067c42d98
Suraj <108755893+surajgajare123@users.noreply.github.com>
2024-11-13 20:29:23 +0530

    Create if_else.java

---
+++ if_else.java
@@ -0,0 +1,12 @@
+public class If_Else_Condition {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        int age = 8;
+        if(age>18){
+            System.out.println("Your adult");
+        }
+        else{
+            System.out.println("your not adult");
+        }
+    }
+}
